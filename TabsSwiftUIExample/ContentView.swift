863c706361de9acc873b3a1bcd722ab951ce067c
John Codeos <johncodeos@gmail.com>
2020-11-07 15:54:44 +0200

    Initial Commit

---
+++ TabsSwiftUIExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabsSwiftUIExample
+//
+//  Created by John Codeos on 11/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TabsSwiftUIExample/TabsSwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabsSwiftUIExampleApp.swift
+//  TabsSwiftUIExample
+//
+//  Created by John Codeos on 11/7/20.
+//
+
+import SwiftUI
+
+@main
+struct TabsSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
